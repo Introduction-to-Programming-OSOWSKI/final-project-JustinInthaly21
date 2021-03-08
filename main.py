d83d855d23f96a345682623a4b8f9ef7bd3b456b
JustinInthaly21 <825134@mystma.org>
2021-03-08 17:52:14 +0000

    final project 1

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-#WRITE YOUR CODE IN THIS FILE
+def times10(x):
+    return x * 10
+
+def firstA(x):
+    if x[0] == "a":
+        return True
+    else:
+        return False
+
+def sizer(n):
+    if n < 10:
+        return "small"
+    elif 9 < n < 101:
+        return "medium"
+    else:
+        return "large"
+
+def doubleWord(w):
+    return w + w 
+
+def penguin(p):
+    
